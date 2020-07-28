6c9cf49c73fac71f24cbfe50458d6067a6418f53
Blaine <baheffron@gmail.com>
2020-07-28 17:05:18 -0400

    proper passing of pinned memory option

--- PulseDataset.py
+++ PulseDataset.py
@@ -1,11 +1,5 @@
-from __future__ import print_function, division
 import os
-import torch
-import numpy as np
 from HDF5Dataset import *
-import matplotlib.pyplot as plt
-from torch.utils.data import Dataset, DataLoader
-from torchvision import transforms, utils
 
 """
 mydl = DataLoader(dataset, batch_size=1, shuffle=False, sampler=None, batch_sampler=None, num_workers=0,
@@ -23,17 +17,25 @@ class PulseDataset2D(HDF5Dataset):
     """Pulse data in the form of ChannelData of size [N,nsamples*2]
     where N is the number of PMTs fired for the M = batch size events"""
 
-    def __init__(self, config, n_per_dir, file_excludes=None, label_name=None, data_cache_size=3, transform=None):
+    def __init__(self, config, n_per_dir,
+                 file_excludes=None,
+                 label_name=None,
+                 data_cache_size=3,
+                 use_pinned=False):
         """
         Args:
-            dirs (list): List of paths to the HDF5 directories.
-            transform (callable, optional): transform to be applied on a sample.
+            config: configuration file object
+            n_per_dir: number of events to use per directory
+            file_excludes: list of file paths to exclude from dataset
+            label_name: name of the table
+            data_cache_size: number of file to hold in memory
+            use_pinned: whether to use pinned memory (for GPU loading)
         """
         paths = [os.path.join(config.base_path, path) for path in config.paths]
         super().__init__(paths, False, False,
                          "*WaveformPairSim.h5", "WaveformPairs",
                          n_per_dir, file_excludes, label_name,
-                         data_cache_size, transform)
+                         data_cache_size, use_pinned)
 
     def __len__(self):
         return len(self.landmarks_frame)
