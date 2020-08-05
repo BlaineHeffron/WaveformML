b14c6e1ddee9dfd6a68625159d303d874a291eb8
Blaine <baheffron@gmail.com>
2020-08-05 13:25:06 -0400

    reverting previous commit

--- PSDDataModule.py
+++ PSDDataModule.py
@@ -4,13 +4,14 @@ from util import DictionaryUtility, ModuleUtility
 
 
 class PSDDataModule(pl.LightningDataModule):
-    def __init__(self, config):
+    def __init__(self, config, device):
         super().__init__()
         self.config = config
         self.ntype = len(self.config.paths)
         self.total_train = self.config.n_train * self.ntype
         self.modules = ModuleUtility(self.config.imports)
         self.dataset_class = self.modules.retrieve_class(self.config.dataset_class)
+        self.device = device
 
     def prepare_data(self):
         # called only on 1 GPU
