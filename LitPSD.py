383bf1bcb3d7aea2d2c01dd71fb85a41d06c3125
Blaine <baheffron@gmail.com>
2020-08-06 11:27:32 -0400

    debugging batch issue

--- LitPSD.py
+++ LitPSD.py
@@ -50,20 +50,23 @@ class LitPSD(pl.LightningModule):
         return optimizer
 
     def training_step(self, batch, batch_idx):
-        for minibatch in batch:
-            predictions = self.model(minibatch[0])
-            loss = self.criterion.forward(predictions, minibatch[1])
+        xs, targets = batch
+        for x, target in zip(xs, targets):
+            predictions = self.model(x)
+            loss = self.criterion.forward(predictions, target)
             result = pl.TrainResult(loss)
             result.log('train_loss', loss)
         return result
 
     def validation_step(self, batch, batch_idx):
-        for minibatch in batch:
-            predictions = self.model(minibatch[0])
-            loss = self.criterion.forward(predictions, minibatch[1])
+        xs, targets = batch
+        import ipdb; ipdb.set_trace()
+        for x, target in zip(xs, targets):
+            predictions = self.model(x)
+            loss = self.criterion.forward(predictions, target)
             result = pl.EvalResult(checkpoint_on=loss)
             pred = argmax(self.softmax(predictions), dim=1)
-            acc = accuracy(pred, minibatch[1], num_classes=self.n_type)
+            acc = accuracy(pred, target, num_classes=self.n_type)
             result.log_dict({'val_loss': loss, 'val_acc': acc})
         return result
 
