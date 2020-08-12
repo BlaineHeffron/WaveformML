4fb80ebed0b8316c3091d70f05da5571541738ef
Blaine <baheffron@gmail.com>
2020-08-12 12:16:43 -0400

    trying converting in __getitem__

--- src/engineering/LitPSD.py
+++ src/engineering/LitPSD.py
@@ -68,7 +68,7 @@ class LitPSD(pl.LightningModule):
         #self.log.debug("Shape of features: {}".format(feat.shape))
         #self.log.debug("Shape of labels: {}".format(targets.shape))
         for c, f, target in zip(coo, feat, targets):
-            c, f, target = self.convert_to_tensors(c, f, target)
+            #c, f, target = self.convert_to_tensors(c, f, target)
             self.log.debug("type of coords: {}".format(c.storage_type()))
             self.log.debug("type of features: {}".format(f.storage_type()))
             self.log.debug("type of labels: {}".format(target.storage_type()))
@@ -84,7 +84,7 @@ class LitPSD(pl.LightningModule):
     def validation_step(self, batch, batch_idx):
         (coo, feat), targets = batch
         for c, f, target in zip(coo, feat, targets):
-            c, f, target = self.convert_to_tensors(c, f, target)
+            #c, f, target = self.convert_to_tensors(c, f, target)
             predictions = self.model([c, f])
             loss = self.criterion.forward(predictions, target)
             result = pl.EvalResult(checkpoint_on=loss)
