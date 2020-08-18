fix weighted average issue

src/engineering/LitPSD.py
@@ -1,7 +1,7 @@
 from pytorch_lightning.metrics.classification import Accuracy, ConfusionMatrix
 from src.engineering.PSDDataModule import *
 from torch.nn import Softmax
-from torch import argmax, max, int32, float32, int64, mean, sum
+from torch import argmax, max, int32, float32, int64, mean, sum, tensor
 import logging
 
 def weight_avg(t,n):
@@ -93,7 +93,8 @@ class LitPSD(pl.LightningModule):
             # self.log.debug("predictions shape is {}".format(predictions.shape))
             loss = self.criterion.forward(predictions, target)
             result = pl.TrainResult(loss)
-            result.log_dict({'train_loss': loss, 'n_batch': target.size[0]})
+            result.log_dict({'train_loss': loss, 'n_batch':
+                tensor(target.shape[0],device=self.device)})
         return result
 
     def validation_step(self, batch, batch_idx):
@@ -105,7 +106,8 @@ class LitPSD(pl.LightningModule):
             result = pl.EvalResult(checkpoint_on=loss, early_stop_on=loss)
             pred = argmax(self.softmax(predictions), dim=1)
             acc = self.accuracy(pred, target)
-            results_dict = {'val_loss': loss, 'val_acc': acc, 'n_batch': target.size[0]}
+            results_dict = {'val_loss': loss, 'val_acc': acc, 'n_batch':
+                    tensor(target.shape[0],device=self.device)}
             if self.n_type > 2:
                 results_dict['val_confusion_matrix'] = self.confusion(pred, target)
             result.log_dict(results_dict, on_epoch=True)