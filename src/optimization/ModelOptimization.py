c3ee506c32cb564aa4d5ed65b47f5532be073b76
Blaine <baheffron@gmail.com>
2020-08-20 15:16:57 -0400

    checkpoint fix

--- src/optimization/ModelOptimization.py
+++ src/optimization/ModelOptimization.py
@@ -133,11 +133,11 @@ class ModelOptimization:
         trainer_args["checkpoint_callback"] = \
             ModelCheckpoint(
                 os.path.join(self.study_dir, "trial_{}".format(trial.number), "{epoch}"),
-                monitor="val_loss")
+                monitor="val_checkpoint_on")
         trainer_args["logger"] = logger
         trainer_args["default_root_dir"] = self.study_dir
         set_default_trainer_args(trainer_args, self.config)
-        trainer_args["early_stop_callback"] = PyTorchLightningPruningCallback(trial, monitor="val_loss")
+        trainer_args["early_stop_callback"] = PyTorchLightningPruningCallback(trial, monitor="val_early_stop_on")
         if trainer_args["profiler"]:
             profiler = SimpleProfiler(output_filename=os.path.join(log_folder, "profile_results.txt"))
             trainer_args["profiler"] = profiler
