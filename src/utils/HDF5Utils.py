1e8bc9d496ed70de52046a85827595b298a9a062
Blaine <baheffron@gmail.com>
2020-08-24 10:51:56 -0400

    trying fix

--- src/utils/HDF5Utils.py
+++ src/utils/HDF5Utils.py
@@ -1,10 +1,20 @@
 import h5py
 
 
-class H5FileHandler(h5py.File):
+class H5FileHandler:
     def __init__(self, path, *args, **kwargs):
+        self.path = path
         try:
-            super().__init__(path, *args, **kwargs)
+            self.h5f = h5py.File(path, *args, **kwargs)
         except Exception as e:
-            print("Opening {} failed:\n".format(path))
+            print("Opening {} failed:\n".format(self.path))
             print(e)
+
+    def __iter__(self, *args, **kwargs):
+        return self.path.__iter__(*args, **kwargs)
+
+    def __getitem__(self, *args, **kwargs):
+        return self.path.__getitem__(*args, **kwargs)
+
+    def __getattr__(self, *args, **kwargs):
+        return self.path.__getattr__(*args, **kwargs)
