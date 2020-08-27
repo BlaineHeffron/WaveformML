cd9c0c09799443e500e7739a298b9587a588c1fa
blaineheffron <bheffron@vols.utk.edu>
2020-08-27 13:08:07 -0400

    adding graph of model

--- src/engineering/LitCallbacks.py
+++ src/engineering/LitCallbacks.py
@@ -1,6 +1,7 @@
+import logging
 from pytorch_lightning.callbacks import EarlyStopping
 from pytorch_lightning.callbacks import LearningRateLogger
-
+from pytorch_lightning.callbacks.base import Callback
 
 
 class PSDCallbacks:
@@ -9,8 +10,19 @@ class PSDCallbacks:
         self.config = config
         self.callbacks = [EarlyStopping('val_early_stop_on', min_delta=.000, verbose=True, mode="min", patience=10)]
         self.callbacks.append(LearningRateLogger())
+        self.callbacks.append(LoggingCallback())
 
     def set_args(self, args):
-        #args["accumulate_grad_batches"] = {5: 2, 20: 3}
+        # args["accumulate_grad_batches"] = {5: 2, 20: 3}
         return args
 
+
+class LoggingCallback(Callback):
+
+    def __init__(self):
+        super().__init__(self)
+        self.log = logging.getLogger(__name__)
+
+    def on_sanity_check_end(self, trainer, pl_module):
+        self.log.debug("adding graph of the model")
+        pl_module.logger.experiment.log_graph(pl_module.model)
