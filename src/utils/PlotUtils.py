53b00c1efba75e55b7c6e521cc3c78f590a462ce
blaineheffron <bheffron@vols.utk.edu>
2020-08-28 13:49:59 -0400

    fix confusion matrix plotting on gpus

--- src/utils/PlotUtils.py
+++ src/utils/PlotUtils.py
@@ -7,20 +7,20 @@ def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix'
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
     fig = plt.figure()
-    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-    plt.title(title)
-    plt.colorbar()
+    fig.imshow(cm, interpolation='nearest', cmap=cmap)
+    fig.title(title)
+    fig.colorbar()
     tick_marks = np.arange(len(classes))
-    plt.xticks(tick_marks, classes, rotation=45)
-    plt.yticks(tick_marks, classes)
+    fig.xticks(tick_marks, classes, rotation=45)
+    fig.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
+        fig.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
-    plt.tight_layout()
-    plt.ylabel('True label')
-    plt.xlabel('Predicted label')
+    fig.tight_layout()
+    fig.ylabel('True label')
+    fig.xlabel('Predicted label')
     return fig
