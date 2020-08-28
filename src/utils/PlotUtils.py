4c46fe672ca77012c955ee7d43b313d0e4044359
blaineheffron <bheffron@vols.utk.edu>
2020-08-28 13:57:21 -0400

    fix plotting issues

--- src/utils/PlotUtils.py
+++ src/utils/PlotUtils.py
@@ -1,25 +1,28 @@
 import itertools
 import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')
 import matplotlib.pyplot as plt
 
 
 def plot_confusion_matrix(cm, classes, normalize=False, title='Confusion matrix', cmap=plt.cm.Blues):
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-    fig = plt.figure(cm, interpolation='nearest', cmap=cmap)
-    fig.title(title)
-    fig.colorbar()
+    fig = plt.figure()
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
     tick_marks = np.arange(len(classes))
-    fig.xticks(tick_marks, classes, rotation=45)
-    fig.yticks(tick_marks, classes)
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
 
     fmt = '.2f' if normalize else 'd'
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-        fig.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
+        plt.text(j, i, format(cm[i, j], fmt), horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
 
-    fig.tight_layout()
-    fig.ylabel('True label')
-    fig.xlabel('Predicted label')
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
     return fig
