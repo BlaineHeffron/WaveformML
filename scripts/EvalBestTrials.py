447af25482d5ef6cc8148a95f9649c64239efcfa
blaineheffron <bheffron@vols.utk.edu>
2020-10-02 14:06:50 -0400

    added script to find best trial and evaluate it

--- scripts/EvalBestTrials.py
+++ scripts/EvalBestTrials.py
@@ -1,12 +1,6 @@
 import argparse
 from pathlib import Path
 from os.path import join, exists, basename, dirname
-
-import sqlite3
-
-import h5py
-
-from scripts.ValidateCombined import json_load, check_file
 from src.utils.SQLUtils import OptunaDB
 from src.utils.TensorBoardUtils import TBHelper, run_evaluation
 from src.utils.util import get_config, get_model_folder
