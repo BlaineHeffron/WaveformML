2fcd0bc338e7ec1536f0f1a94f0f295934b2b6fb
blaineheffron <bheffron@vols.utk.edu>
2020-12-02 17:55:55 -0500

    fix target retrieval

--- src/engineering/LitZ.py
+++ src/engineering/LitZ.py
@@ -1,7 +1,9 @@
 import spconv
 from src.models.SingleEndedZConv import SingleEndedZConv
 from src.engineering.PSDDataModule import *
-#from src.evaluation.ZEvaluator import ZEvaluator
+
+
+# from src.evaluation.ZEvaluator import ZEvaluator
 
 
 class LitZ(pl.LightningModule):
@@ -24,7 +26,7 @@ class LitZ(pl.LightningModule):
         self.model = SingleEndedZConv(self.config)
         self.criterion_class = self.modules.retrieve_class(config.net_config.criterion_class)
         self.criterion = self.criterion_class(*config.net_config.criterion_params)
-        #self.evaluator = ZEvaluator(config)
+        # self.evaluator = ZEvaluator(config)
 
     def forward(self, x, *args, **kwargs):
         return self.model(x)
@@ -59,12 +61,21 @@ class LitZ(pl.LightningModule):
                 return [optimizer], [scheduler]
         return optimizer
 
+    def _format_target_and_prediction(self, pred, coords, target, batch_size):
+        target_tensor = spconv.SparseConvTensor(target.unsqueeze(1), coords[:, self.model.permute_tensor],
+                                                self.model.spatial_size, batch_size)
+        target_tensor = target_tensor.dense()
+        # set output to 0 if there was no value for input
+        zero_coords = (target_tensor == 0).nonzero()
+        pred[zero_coords] = 0
+        return pred, target_tensor
+
     def training_step(self, batch, batch_idx):
         (c, f), target = batch
         predictions = self.model([c, f])
         batch_size = c[-1, -1] + 1
-        target_tensor = spconv.SparseConvTensor(target, c[:, self.model.permute_tensor], self.model.spatial_size, batch_size)
-        loss = self.criterion.forward(predictions, target_tensor.dense())
+        predictions, target_tensor = self._format_target_and_prediction(predictions, c, target, batch_size)
+        loss = self.criterion.forward(predictions, target_tensor)
         self.log('train_loss', loss, on_epoch=True, prog_bar=True, logger=True)
         return loss
 
@@ -72,8 +83,8 @@ class LitZ(pl.LightningModule):
         (c, f), target = batch
         predictions = self.model([c, f])
         batch_size = c[-1, -1] + 1
-        target_tensor = spconv.SparseConvTensor(target, c[:, self.model.permute_tensor], self.model.spatial_size, batch_size)
-        loss = self.criterion.forward(predictions, target_tensor.dense())
+        predictions, target_tensor = self._format_target_and_prediction(predictions, c, target, batch_size)
+        loss = self.criterion.forward(predictions, target_tensor)
         results_dict = {'val_loss': loss}
         self.log_dict(results_dict, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         return results_dict
@@ -82,12 +93,11 @@ class LitZ(pl.LightningModule):
         (c, f), target = batch
         predictions = self.model([c, f])
         batch_size = c[-1, -1] + 1
-        target_tensor = spconv.SparseConvTensor(target, c[:, self.model.permute_tensor], self.model.spatial_size, batch_size)
-        loss = self.criterion.forward(predictions, target_tensor.dense())
+        predictions, target_tensor = self._format_target_and_prediction(predictions, c, target, batch_size)
+        loss = self.criterion.forward(predictions, target_tensor)
         results_dict = {'test_loss': loss}
-        #if not self.evaluator.logger:
+        # if not self.evaluator.logger:
         #    self.evaluator.logger = self.logger
-        #self.evaluator.add(batch, predictions)
+        # self.evaluator.add(batch, predictions)
         self.log_dict(results_dict, on_epoch=True, logger=True)
         return results_dict
-
