a80e2c6a2e6b8e0b036274a776c19db48832c11f
blaineheffron <bheffron@vols.utk.edu>
2020-12-14 16:04:37 -0500

    reduce memory usage

--- src/evaluation/ZEvaluator.py
+++ src/evaluation/ZEvaluator.py
@@ -27,13 +27,13 @@ class ZEvaluator:
         self.z_bin_edges = get_bins(self.zmin, self.zmax, self.n_bins)
         self.mult_bin_edges = get_bins(0.5, self.nmult + 0.5, self.nmult)
         self.hascal = False
-        self.SE_dead_pmts = [1, 0, 2, 4, 6, 7, 9, 10, 12, 13, 16, 19, 20, 21, 22, 24, 26, 27, 34, 36, 37, 43, 46, 48,
+        SE_dead_pmts = [1, 0, 2, 4, 6, 7, 9, 10, 12, 13, 16, 19, 20, 21, 22, 24, 26, 27, 34, 36, 37, 43, 46, 48,
                              55,
                              54, 56, 58, 65, 68, 72, 80, 82, 85, 88, 93, 95, 97, 96, 105, 111, 112, 120, 122, 137, 138,
                              139, 141, 147, 158, 166, 173, 175, 188, 195, 215, 230, 243, 244, 245, 252, 255, 256, 261,
                              273, 279, 282]
         self.seg_status = np.zeros((self.nx, self.ny), dtype=np.float32)  # 0 for good, 0.5 for single ended, 1 for dead
-        self.set_SE_segs()
+        self.set_SE_segs(SE_dead_pmts)
         if calgroup is not None:
             self.hascal = True
             if "PROSPECT_CALDB" not in os.environ.keys():
@@ -47,8 +47,8 @@ class ZEvaluator:
         # self.metrics = []
         self._init_results()
 
-    def set_SE_segs(self):
-        for pmt in self.SE_dead_pmts:
+    def set_SE_segs(self, SE_dead_pmts):
+        for pmt in SE_dead_pmts:
             r = pmt % 2
             seg = int((pmt - r) / 2)
             x = seg % 14
@@ -58,7 +58,7 @@ class ZEvaluator:
     def _init_results(self):
         # metric_names = ["energy", "multiplicity", "true_z", "pred_z"]
         # metric_params = [[0.0, 10.0, 40], [0.5, 10.5, 10], [-600.,600.,40],[-600.,600.,40]]
-        i = 0
+        #i = 0
         # for name in metric_names:
         #    self.metrics.append(MetricAggregator(name, *metric_params[i], ["positron"]))
         #    i += 1
