37ec319a5331ebe93c6a6a3c39c440a9602b23c1
blaineheffron <bheffron@vols.utk.edu>
2021-02-08 12:09:38 -0500

    energy range for plotting

--- src/evaluation/ZEvaluator.py
+++ src/evaluation/ZEvaluator.py
@@ -29,7 +29,7 @@ class ZEvaluatorBase:
         self.n_err_bins = 50
         self.error_low = -1000.
         self.error_high = 1000.
-        self.E_high = 9.0
+        self.E_high = 12.0
         self.E_low = 0.0
         self.spatial_size = np.array([14, 11])
         self.permute_tensor = torch.LongTensor([2, 0, 1])  # needed because spconv requires batch index first
