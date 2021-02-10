4f5dfc3433420a2fec757ebeb4abd2b3fdb99c4a
blaineheffron <bheffron@vols.utk.edu>
2021-02-10 16:55:28 -0500

    fix nopython issue

--- src/utils/SparseUtils.py
+++ src/utils/SparseUtils.py
@@ -824,7 +824,9 @@ def E_basic_prediction(coo, E, PE0, PE1, z, seg_status, light_pos_curves, light_
             if PE0[batch] == 0 and PE1[batch] == 0:
                 continue
             elif PE0[batch] != 0 and PE1[batch] != 0:
-                print("error: seg status is incongruent with PE0, PE1, for segment {0}, {1}".format(x,y))
+                print("error: seg status is incongruent with PE0, PE1, for segment ")
+                print(x)
+                print(y)
             logR = lin_interp_inverse(light_pos_curves[x,y], z[batch])
             if PE0[batch] == 0:
                 P0 = PE1[batch]/exp(logR)
