acc0675674bf165f498f34c4bd09c4669b5a749a
blaineheffron <bheffron@vols.utk.edu>
2021-02-15 15:44:59 -0500

    use EZ algorithm if version > 0

--- src/models/SingleEndedZConv.py
+++ src/models/SingleEndedZConv.py
@@ -2,7 +2,7 @@ import logging
 from torch import nn, LongTensor
 import spconv
 from numpy import array
-from src.models.SPConvBlocks import SparseConv2DForZ, Pointwise2DForZ
+from src.models.SPConvBlocks import SparseConv2DForZ, Pointwise2DForZ, SparseConv2DForEZ
 from src.utils.util import ModuleUtility
 from src.utils.util import DictionaryUtility
 
@@ -19,12 +19,22 @@ class SingleEndedZConv(nn.Module):
         self.modules = ModuleUtility(self.net_config.imports)
         if not hasattr(self.net_config, "algorithm"):
             setattr(self.net_config, "algorithm", "conv")
+        if hasattr(self.net_config, "version"):
+            self.version = self.net_config.version
+        else:
+            self.version = 0
         if self.net_config.algorithm == "conv":
-            self.model = SparseConv2DForZ(self.nsamples*2, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
+            if self.version == 0:
+                self.model = SparseConv2DForZ(self.nsamples*2, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
+            else:
+                self.model = SparseConv2DForEZ(self.nsamples*2, out_planes=1, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
         elif self.net_config.algorithm == "point":
             self.model = Pointwise2DForZ(self.nsamples*2, **DictionaryUtility.to_dict(self.net_config.hparams.point))
         elif self.net_config.algorithm == "features":
-            self.model = SparseConv2DForZ(self.nsamples, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
+            if self.version == 0:
+                self.model = SparseConv2DForZ(self.nsamples, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
+            else:
+                self.model = SparseConv2DForEZ(self.nsamples, out_planes=1, **DictionaryUtility.to_dict(self.net_config.hparams.conv))
         self.spatial_size = array([14, 11])
         self.permute_tensor = LongTensor([2, 0, 1])  # needed because spconv requires batch index first
 
