3ced9d95a4cefaa723f67bc09215593af8e5274e
blaineheffron <bheffron@vols.utk.edu>
2021-03-04 13:13:28 -0500

    fix expand dims call

--- src/evaluation/ZEvaluator.py
+++ src/evaluation/ZEvaluator.py
@@ -455,7 +455,7 @@ class ZEvaluatorPhys(ZEvaluatorBase, PhysCoordEvaluator):
             E = f[:, 0] * self.E_scale
             E = self.get_dense_matrix(E, c)
         else:
-            E = np.expand_dims(E, dim=1)
+            E = np.expand_dims(E, axis=1)
         z_deviation_with_E(pred[:, 0, :, :], targ[:, 0, :, :], self.results["seg_mult_mae"][0],
                            self.results["seg_mult_mae"][1], self.results["z_mult_mae_dual"][0],
                            self.results["z_mult_mae_dual"][1], self.results["z_mult_mae_single"][0],
